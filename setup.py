e13fdf195f21888b2f35eb05d3227137862f34d5
yacchin1205 <yazawa@yzwlab.net>
2015-12-13 21:28:09 +0900

    Add mqtt-itunes to setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,10 @@ setup(name='MQTTAdapter',
       author_email='yazawa@yzwlab.net',
       url='https://github.com/yacchin1205/mqtt-adapters',
       packages=['mqttadapters'],
-      install_requires=['paho-mqtt', 'zeroconf', 'ipaddress', 'requests', 'phue'],
+      install_requires=['paho-mqtt', 'zeroconf', 'ipaddress', 'requests',
+                        'phue', 'py-applescript'],
       entry_points={'console_scripts':
                     ['mqtt-irkit=mqttadapters.irkit:main',
-                     'mqtt-hue=mqttadapters.hue:main']},
+                     'mqtt-hue=mqttadapters.hue:main',
+                     'mqtt-itunes=mqttadapters.itunes:main']},
       )
