99c0886402325743f0dd40b995c56b12743c82a4
yacchin1205 <yazawa@yzwlab.net>
2017-01-03 01:21:33 +0000

    Add mqtt-grovepi command

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='MQTTAdapter',
-      version='0.1',
+      version='0.1.1',
       description='MQTT Adapters for Smart home devices',
       author='Satoshi Yazawa',
       author_email='yazawa@yzwlab.net',
@@ -14,5 +14,6 @@ setup(name='MQTTAdapter',
       entry_points={'console_scripts':
                     ['mqtt-irkit=mqttadapters.irkit:main',
                      'mqtt-hue=mqttadapters.hue:main',
-                     'mqtt-itunes=mqttadapters.itunes:main']},
+                     'mqtt-itunes=mqttadapters.itunes:main',
+                     'mqtt-grovepi=mqttadapters.grove:main']},
       )
